27ce8926d531e4dcbaf260b65e7d756266250ce5
Bourbon Kung <40812693+Bourbon0212@users.noreply.github.com>
2019-05-26 10:42:58 +0800

    0526(V5.8)
    Time Weight Algorithm Update!!

--- get_functions.py
+++ get_functions.py
@@ -80,25 +80,65 @@ def get_yymm(history):
     print("YYMM DATA SUCCESS!")
     return yymm_data
 
-
 def compute_weight(k):
     '''
-        Inverse func of: [ k + (k - 1) ] - SIGMA(i = 1, w + 2)[i] >= 1
+        Find the argmax(w) of the corresponding k predicted points
+
+        Theory:
+        
+            (i) When hasConflict --> a + bw versus c + dw, and a < c, b > d, where
+                 |￣                x                                 x+w      ￣|
+                 |　  a = x,  b = SIGMA(k - i),  c = x + omega,  d = SIGMA i  　 |
+                 |　               i=0                                i=1     　 |
+                 |__                                                      　　 __|
+
+            (ii) Next, w = argmax omega{ Wp }, which means,
+    　　                      p
+            　        w = -(a - c) / (b - d) | b - d = 1, c - a = omega by altering different gap, x
     '''
-    import math
+    ### Part 1. Fing all the possible score pairs
+    total_pair = [] # all the possible scor pairs (Sigmw i, Sigma k)
+    num = list(range(1, k + 1))
 
-    w = (math.sqrt(16 * k - 15) - 5) / 2
-    w = int(w) #floor the float above(w)
-    return w if w > 0 else 1
+    for i in range(1, 1 + k):
+        row_of_i = []
+        # 1. find the min sigma k, for given sigma i
+        start = 0
+        for k in range(i):
+            start += num[k]
 
-''' Test of Time Weight
-temp = compute_weight(1)
-for k in range(1, 1000):
+        # 2. find the max sigma k, for given sigma i
+        end = 0
+        for k in range(1, i + 1):
+            end += num[-k]
 
-    if compute_weight(k) == temp:
-        print('w = ' + str(compute_weight(k)) + ', k =' + str(k))
-    else:
-        print()
-        print('w = ' + str(compute_weight(k)) + ', k =' + str(k))
-        temp = compute_weight(k)
-'''
+        # 3. List all the possible sigma k
+        ready = list(range(start, end + 1))
+
+        for d in ready:
+            temp = (i, d)
+            row_of_i.append(temp)
+
+        # 4. Append all possible pairs, for given Sigma i
+        total_pair.append(row_of_i)
+
+    ### Part 2. Find the all the omegas and W = argmax(w)
+    omega_list = [] # List to contains all omegas
+    maxk = len(total_pair)
+
+    for l in range(maxk): # l refers to the current row, x in the discussion
+        target = total_pair[l][-1] # Given sigma i, the biggest sigma k
+        hasConflict = True
+        m = l + 1 # m refers to the following rows after l, x + omega in the discussion
+
+        while hasConflict and m < maxk: # If it is possible to have conflict
+            if target[1] <= total_pair[m][0][1]: # If the target's max sigma k _lt_ the min of the Mth's, there's no conflict
+                hasConflict = False
+            else:
+                omega = m - l # b/c: m - l = (x + omega) - x = omega
+                omega_list.append(omega)
+
+            m += 1 # Search for the next row
+
+
+    return 1 if len(omega_list) == 0 else (max(omega_list))
